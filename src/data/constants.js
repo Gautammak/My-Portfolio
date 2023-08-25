3cfd09203de85b1b57b1e46fca86f83e5193eb0b
Gautammak <gautammakavana2001@gmail.com>
2023-08-25 18:04:07 +0530

    my-portfolio

--- src/data/constants.js
+++ src/data/constants.js
@@ -12,7 +12,7 @@ export const Bio = {
       "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
     github: "https://github.com/Gautammak",
     resume:
-      "https://drive.google.com/file/d/1nVaF0GeGhMLr0ufyhhFHfwSHqpaR6I0i/view?usp=sharing",
+      "https://drive.google.com/file/d/1HjGMO6fTkW6uc6mpYUjDpSKMWuKAaQ1r/view?usp=sharing",
     linkedin: "https://www.linkedin.com/in/gautam-makavana-24a5881b0/",
     // twitter: "https://twitter.com/RishavChanda",
     // insta: "https://www.instagram.com/rishav_chanda/",
